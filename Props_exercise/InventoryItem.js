8b2627ad22173f88cba4de3822cc481573fe907e
Ruth <ruthleeyuxin@gmail.com>
2025-10-13 00:37:17 -0700

    modify inventoryitem.js

--- Props_exercise/InventoryItem.js
+++ Props_exercise/InventoryItem.js
@@ -1,4 +1,4 @@
-function InventoryItem({ itemName, type, quantity = 0, price = 0 }) {
+function InventoryItem({ name, type, quantity = 0, price = 0 }) {
   const lowStock = 5;
   const highValue = 1000;
 
@@ -7,7 +7,7 @@ function InventoryItem({ itemName, type, quantity = 0, price = 0 }) {
   return (
     <div className="inventory-item">
       <h2>
-        {itemName} ({type})
+        {name} ({type})
       </h2>
       {quantity < lowStock && (
         <Message>
