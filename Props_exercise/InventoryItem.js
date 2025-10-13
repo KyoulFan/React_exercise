11a7ab999d7f50d71fbb480df870fd69ae7228fe
Ruth <ruthleeyuxin@gmail.com>
2025-10-13 00:34:31 -0700

    debug component InventoryItems.js

--- Props_exercise/InventoryItem.js
+++ Props_exercise/InventoryItem.js
@@ -1,27 +1,28 @@
-function InventoryItem({name, type, quantity = 0, price = 0 }) {
+function InventoryItem({ itemName, type, quantity = 0, price = 0 }) {
   const lowStock = 5;
   const highValue = 1000;
 
   const totalValue = quantity * price;
+
+  return (
+    <div className="inventory-item">
+      <h2>
+        {itemName} ({type})
+      </h2>
+      {quantity < lowStock && (
+        <Message>
+          <p>
+            <span>⚠️</span>Low Stock! {quantity} remained.
+          </p>
+        </Message>
+      )}
+      {totalValue > highValue && (
+        <Message>
+          <p>
+            <span>💰</span>High value - consider extra protection!
+          </p>
+        </Message>
+      )}
+    </div>
+  );
 }
-return (
-  <div className="inventory-item">
-    <h2>
-      {name}({type})
-    </h2>
-    {quantity < lowStock && (
-      <Message>
-        <p>
-          <span>⚠️</span>Low Stock! {quantity} remained.
-        </p>
-      </Message>
-    )}
-    {totalValue > highValue && (
-      <Message>
-        <p>
-          <span>💰</span>High value - consider extra protection!
-        </p>
-      </Message>
-    )}
-  </div>
-);
